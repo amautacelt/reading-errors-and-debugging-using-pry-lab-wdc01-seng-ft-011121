ef4976e93fe7ff634baf208761ca303968f3465c
C. L <amautacelt@gmail.com>
2020-12-27 02:43:58 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -2,11 +2,11 @@
 require 'pry'
 
 def start_game(player1, player2)
-  puts "Hello #{player1} & #{player2}"
+  "Hello #{player1} & #{player2}"
 end
 
 def play_game(player1, player2)
-  puts "#{player1} is better than #{player2}"
+  "#{player1} is better than #{player2}"
 end
 
 start_game("Rocky", "Bullwinkle")
